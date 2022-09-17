bebce4de925ff28bef3d387b9b38f41c687e56aa
Randy Efan <randyrandyrej8@gmail.com>
2022-09-17 10:18:13 +0700

    Initial Commit

---
+++ CleanArchitecture-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture-UIKit
+//
+//  Created by Randy Efan Jayaputra on 17/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
