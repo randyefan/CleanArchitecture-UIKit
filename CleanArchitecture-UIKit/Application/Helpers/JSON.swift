cc5b74e2bb2d579edee9d8a39c10c6c106422872
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2022-09-18 11:04:41 +0700

    add helpers

---
+++ CleanArchitecture-UIKit/Application/Helpers/JSON.swift
@@ -0,0 +1,27 @@
+//
+//  JSON.swift
+//  CleanArchitecture-UIKit
+//
+//  Created by Randy Efan Jayaputra on 18/09/22.
+//
+
+import Foundation
+
+class JSON {
+    
+    func newJSONDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+            decoder.dateDecodingStrategy = .iso8601
+        }
+        return decoder
+    }
+    
+    func newJSONEncoder() -> JSONEncoder {
+        let encoder = JSONEncoder()
+        if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+            encoder.dateEncodingStrategy = .iso8601
+        }
+        return encoder
+    }
+}
